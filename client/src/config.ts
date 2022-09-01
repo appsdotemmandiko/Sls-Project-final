1e303176715cbdeb3bee4a549179458031334118
appsdotemmandiko <apps.emmandiko@gmail.com>
2022-09-01 14:11:39 +0300

    Final project with x-tracing

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'f';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-x5epuxxa.us.auth0.com',
+  clientId: 'dbWhcKaAgc8fvE85ry7MHJ6LFdrWY2y1',
+  callbackUrl: 'http://localhost:3000/callback'
+};
